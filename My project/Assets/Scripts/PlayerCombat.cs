618dba03d8325864af68ff62211d28a77e393042
Michael.L <126206742+MLiondis@users.noreply.github.com>
2024-06-18 10:34:13 +1000

    started attack damage

--- My project/Assets/Scripts/PlayerCombat.cs
+++ My project/Assets/Scripts/PlayerCombat.cs
@@ -6,8 +6,13 @@ public class PlayerCombat : MonoBehaviour
 {
 
     public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
 
-    // Update is called once per frame
+
+  // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -19,5 +24,20 @@ public class PlayerCombat : MonoBehaviour
     void Attack()
     {
         animator.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers); 
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
