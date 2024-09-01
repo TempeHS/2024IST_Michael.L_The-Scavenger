ce594877836bd5267a892ecb2ba5d2b395b94683
Michael.L <126206742+MLiondis@users.noreply.github.com>
2024-09-01 19:38:35 +1000

    added camera movement super sigma go plus ultra!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ My project/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+
+
+    public GameObject virtualCam;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+}
