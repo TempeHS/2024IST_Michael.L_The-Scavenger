76d691f5a7a0bfd805984b918bc6edfec7e0d585
Michael.L <126206742+MLiondis@users.noreply.github.com>
2024-09-26 14:40:39 +1000

    FINAL FLASSHSHHSHHS
    V2

--- My project/Assets/Scripts/Points.cs
+++ My project/Assets/Scripts/Points.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class Points : MonoBehaviour
 {
     public static Points instance;
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
     public int score = 0;
 
     void Awake()
